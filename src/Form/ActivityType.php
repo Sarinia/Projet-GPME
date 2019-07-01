633a2b97cf825a1e28aa67d9df5f9a5577011c91
compain <compain.aurelien@gmail.com>
2019-07-01 17:32:50 +0200

    maj

--- src/Form/ActivityType.php
+++ src/Form/ActivityType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\Activity;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,9 +14,10 @@ class ActivityType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('number')
-            ->add('name')
-            ->add('skill')
+            ->add('number', TextType::class, ['label' => 'Numéro :'])
+            ->add('name', TextType::class, ['label' => 'Nom :'])
+            ->add('skill', TextType::class, ['label' => 'Compétence :'])
+            ->add('save', SubmitType::class, ['label' => 'Enregistrer'],['attr' => ['class' => 'btn btn-primary']]);
         ;
     }
 
