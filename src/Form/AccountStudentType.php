652332c2999f01e1e28e6295810b9c1d95f9a7ee
Sarinia <compain.aurelien@gmail.com>
2019-07-02 23:02:50 +0200

    maj

--- src/Form/AccountStudentType.php
+++ src/Form/AccountStudentType.php
@@ -8,6 +8,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -16,7 +17,7 @@ class AccountStudentType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('candidateNb')
+            ->add('candidateNb', TextType::class,['required' => false])
             ->add('birthDate', DateType::class, ['widget'=> 'single_text'])
             // ->add('createdAt')
             // ->add('establishment')
--- src/Migrations/Version20190702193357.php
+++ src/Migrations/Version20190702193357.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20190702072638 extends AbstractMigration
+final class Version20190702193357 extends AbstractMigration
 {
     public function getDescription() : string
     {
